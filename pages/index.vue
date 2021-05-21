c0168d49ab82407770bc24ef37341472df8cb7a8
Ryan Grunest <ryan.grunest@rexelusa.com>
2021-05-21 14:31:08 -0600

    asdf

---
+++ pages/index.vue
@@ -0,0 +1,25 @@
+<template>
+  <v-row justify="center" align="center">
+		<v-col :cols="12" :sm="8">
+			<v-card
+				elevation="8"
+				outlined
+				shaped
+			>
+				<v-card-title>Hi Noodle :)</v-card-title>
+			</v-card>
+		</v-col>
+  </v-row>
+</template>
+
+<script>
+import Logo from '~/components/Logo.vue'
+import VuetifyLogo from '~/components/VuetifyLogo.vue'
+
+export default {
+  components: {
+    Logo,
+    VuetifyLogo
+  }
+}
+</script>
